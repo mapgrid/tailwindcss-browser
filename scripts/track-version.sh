ed483ceb040be400ac7c44e7e814320f85f9633c
mapgrid <noreply@mapgrid.org>
2020-01-13 11:33:36 +0100

    logging

--- scripts/track-version.sh
+++ scripts/track-version.sh
@@ -3,8 +3,11 @@ VERSION=$(yarn info . version --silent)
 UPSTREAM=$(yarn info tailwindcss version --silent)
 
 if [[ "$VERSION" == "$UPSTREAM" ]]; then
+    echo "Upstream version ($UPSTREAM) matches $VERSION. Exiting."
     exit
 fi
+
+echo "New Upstream version ($UPSTREAM). Publishing..."
 
 yarn version --new-version "$UPSTREAM"
 yarn publish --new-version "$UPSTREAM"
