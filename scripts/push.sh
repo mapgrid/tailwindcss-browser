2dcd9816d977c0e4d373f626cd9a441477af1ef8
mapgrid <noreply@mapgrid.org>
2020-01-30 15:18:08 +0100

    user

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 VERSION=$(yarn info . version --silent)
 
+git config user.email "noreply@mapgrid.org"
+git config user.name "mapgrid"
 git add package.json
 git commit -m "$VERSION"
 git push
